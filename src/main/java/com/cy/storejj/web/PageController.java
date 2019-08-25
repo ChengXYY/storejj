62dc9fb150871042ab3c8c124c5a50ef4e38dc31
ChengXYY <chengxyy@qq.com>
2019-08-25 10:02:55 +0800

    update

--- src/main/java/com/cy/storejj/web/PageController.java
+++ src/main/java/com/cy/storejj/web/PageController.java
@@ -2,7 +2,12 @@ package com.cy.storejj.web;
 
 import com.alibaba.fastjson.JSONObject;
 import com.cy.storejj.config.WebConfig;
+import com.cy.storejj.model.Category;
+import com.cy.storejj.model.Product;
+import com.cy.storejj.service.CategoryService;
+import com.cy.storejj.service.ProductService;
 import com.cy.storejj.service.SitePageService;
+import org.hibernate.validator.constraints.EAN;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class PageController extends WebConfig {
@@ -20,9 +28,20 @@ public class PageController extends WebConfig {
 
     @Autowired
     private SitePageService sitepageService;
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private ProductService productService;
 
     @RequestMapping(value = {"/", "", "/index", "/index/"})
     public String index(ModelMap modelMap){
+        Map<String, Object> filter = new HashMap<>();
+        List<Category> categoryList = categoryService.getList(filter);
+        filter.put("page", 0);
+        filter.put("pageSize", 9);
+        List<Product> productList = productService.getList(filter);
+        pageData.put("category", categoryList);
+        pageData.put("product", productList);
         pageData.put("title", systemTitle);
         pageData.put("topflag", "index");
         modelMap.addAttribute("page", pageData);
